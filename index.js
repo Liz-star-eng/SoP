dc2a48ee6a8433d89daff2a1fe3ba4aa1233aee6
LizAfriyie <elizabethafriyie277@gmail.com>
2023-08-23 04:39:37 -1200

    my initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function openUpdate(evt, releaseMonth) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabContent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(releaseMonth).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
